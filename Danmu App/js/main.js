13a823123519ba382f059cbb09a11662660cf736
Zhongwei Zhou <zhouzhongwei1986@sina.com>
2016-11-27 22:57:41 +0800

    update danmu app project

--- Danmu App/js/main.js
+++ Danmu App/js/main.js
@@ -75,7 +75,7 @@
 	    var getRandomColor = function() {
 	        return '#' + (function(h) {
 	            return new Array(7 - h.length).join("0") + h
-	        })((Math.random() * 0x2000000 << 0).toString(16))
+	        })((Math.random() * 0x1000000 << 0).toString(16))
 	    }
 
 	    var getAndRun = function() {
