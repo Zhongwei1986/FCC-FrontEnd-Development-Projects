a7a91cde51af9907a32d3458cc48d0682aa8e4cd
Zhongwei Zhou <zhouzhongwei1986@sina.com>
2017-01-11 01:58:04 +0800

    update  HTML/CSS/JS files

--- RandomQuoteMachine/main.js
+++ RandomQuoteMachine/main.js
@@ -1,73 +1,39 @@
-/*
-  Code by Gabriel Nunes
-*/
+/* Code by Wayne(Zhou Zhongwei)*/
 
-function inIframe () { try { return window.self !== window.top; } catch (e) { return true; } }
-
-var colors = ['#16a085', '#27ae60', '#2c3e50', '#f39c12', '#e74c3c', '#9b59b6', '#FB6964', '#342224', "#472E32", "#BDBB99", "#77B1A9", "#73A857"];
-var currentQuote = '', currentAuthor = '';
-function openURL(url){
-  window.open(url, 'Share', 'width=550, height=400, toolbar=0, scrollbars=1 ,location=0 ,statusbar=0,menubar=0, resizable=0');
-}
+//Define a function that gets JSON data from external API
+//I used an Free API,its url: https://market.mashape.com/andruxnet/random-famous-quotes 
 function getQuote() {
-  $.ajax({
-    headers: {
-      "X-Mashape-Key": "OivH71yd3tmshl9YKzFH7BTzBVRQp1RaKLajsnafgL2aPsfP9V",
-      Accept: "application/json",
-      "Content-Type": "application/x-www-form-urlencoded"
-    },
-    url: 'https://andruxnet-random-famous-quotes.p.mashape.com/cat=',
-    success: function(response) {
-      var r = JSON.parse(response);
-      currentQuote = r.quote;
-      currentAuthor = r.author;
-      if(inIframe())
-      {
-        $('#tweet-quote').attr('href', 'https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' + encodeURIComponent('"' + currentQuote + '" ' + currentAuthor));
-        $('#tumblr-quote').attr('href', 'https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes,freecodecamp&caption='+encodeURIComponent(currentAuthor)+'&content=' + encodeURIComponent(currentQuote));
-      }
-      $(".quote-text").animate({
-          opacity: 0
-        }, 500,
-        function() {
-          $(this).animate({
-            opacity: 1
-          }, 500);
-          $('#text').text(r.quote);
-        });
-
-      $(".quote-author").animate({
-          opacity: 0
-        }, 500,
-        function() {
-          $(this).animate({
-            opacity: 1
-          }, 500);
-          $('#author').html(r.author);
-        });
-
-      var color = Math.floor(Math.random() * colors.length);
-      $("html body").animate({
-        backgroundColor: colors[color],
-        color: colors[color]
-      }, 1000);
-      $(".button").animate({
-        backgroundColor: colors[color]
-      }, 1000);
-    }
-  });
+    //make an ajax request,then append content to related elements
+    $.ajax({        
+        url: "https://andruxnet-random-famous-quotes.p.mashape.com/?cat=movies",
+        contentType: "application/x-www-form-urlencoded",
+        method: "POST",
+        headers: {
+            'X-Mashape-Key': 'ihe54TwpLKmshUBlHlsH68cHNODhp1cqQcOjsn2xyqIQXcc2Cj',
+        },
+        accept: 'application/json',
+    }).done(function(data) {
+        data = JSON.parse(data);
+        var quote = data.quote;
+        var author = data.author;
+        $(".m-quote").text(quote);
+        $("#author").text(author);
+    });
 }
+
+//Initial the page when doucument ready
+
 $(document).ready(function() {
-  getQuote();
-  $('#new-quote').on('click', getQuote);
-  $('#tweet-quote').on('click', function() {
-    if(!inIframe()) {
-      openURL('https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' + encodeURIComponent('"' + currentQuote + '" ' + currentAuthor));
-    }
-  });
-  $('#tumblr-quote').on('click', function() {
-    if(!inIframe()) {
-      openURL('https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes,freecodecamp&caption='+encodeURIComponent(currentAuthor)+'&content=' + encodeURIComponent(currentQuote));
-    }
-  });
+    getQuote();
+});
+
+//Add event listener to the button "New Quote"
+
+$("#new-quote").click(function(e) {
+    
+});
+
+//Share the quote to Twitter
+$("#twitter").click(function() {
+    // body...
 });
