86a6f12d76b58e655eaae5cf68b8f019399232fd
Zhongwei1986 <zhouzhongwei1986@sina.com>
2017-02-05 23:51:33 +0800

    update

--- LocalWeather/app.js
+++ LocalWeather/app.js
@@ -1,40 +1,62 @@
-//数据填充
-
-//天气信息相关DOM
+//全局变量
+var $window = $(window);
 var $citySearch = $('#city-search');
 var $searchBtn = $('#search-btn');
 var $cityName = $('#city-name');
-var $updateTime = $('#update-time');
-var $currenTemp = $('#current-temp');
-var $windDirection = $('#wind-direction');
-var $windStrength = $('#wind-strength');
-var $humidity = $('#humidity');
-var $date = $("#date");
-var $week = $('#week');
-var weatherInfo = $('#weather-info');
-var windToday = $('#wind-today');
-var dressingIndex = $('#dressing-index');
-var dressingAdvice = $('#dressing-advice');
-var uvIndex = $('#uv-index');
-var comfortIndex = $('#comfort-index');
-var washIndex = $('#wash-index');
-var travelIndex = $('#travel-index');
-var exerciseIndex = $('#exercise-index');
-var dryingIndex = $('#drying-index');
+var $currentWeatherArr = [
+  $('#current-temp'), $('#wind-direction'), $('#wind-strength'),
+  $('#humidity'), $('#update-time'),
+];
+var $todayWeatherArr = [
+  $("#date"), $("#date"), $("#temperature"),
+  $('#weather-info'), $('#wind-today'), $('#dressing-index'),
+  $('#dressing-advice'), $('#uv-index'), $('#comfort-index'),
+  $('#wash-index'), $('#travel-index'), $('#exercise-index'),
+  $('#drying-index')
+];
+var appkey = '3e71af0ff0937226f9690f000ce27c3f';
+var cities = [];
 
+/**
+ * getCityResults 获得城市列表
+ */
+function getCityResults() {
+  var preUrl = "http://v.juhe.cn/weather/citys";
+  var getUrl = preUrl + "?key=" + appkey;
+  $.getJSON(getUrl, function(data) {;
+    data = JSON.parse(data);
+    cities = data.result;
+  });
+  return cities;
+}
 
 
 /**
  * updateText 更新DOM节点的文本
  * 
- * @param {Object} el
- * @param {String} str
+ * @param {Object} el 元素节点
+ * @param {String} str 文本字符串
  */
 function updateText(el, str) {
   el.text(str);
 }
 
-$searchBtn.on('click', function() {
-  var url = "http://v.juhe.cn/weather/index";
-  var cityName;
-});
+/**
+ * getWeatherByCityName由城市名获得天气数据
+ * 
+ * @param {string} cityname 城市名，汉字形式
+ */
+function getWeatherByCityName(cityname) {
+  var preUrl = "http://v.juhe.cn/weather/index";
+  cityname = encodeURI(cityname);
+  var getUrl = api + '?format=2&cityname=' + cityname + "&key=" + appkey;
+  $.getJSON(getUrl, function(data) {
+      data = JSON.parse(data);
+      var currentWeather = data.result.sk;
+      var todayWeather = data.result.today;
+      var futureWeather = data.result.future;
+      for (var i = 0, max = currentWeather.length; i < max; i++) {
+        updateText($currentWeatherArr[i], currentWeather[index]
+        }
+      });
+  }
