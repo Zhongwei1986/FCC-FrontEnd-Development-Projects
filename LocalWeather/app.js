c8cc7ea96ea4110c386a55e6f9fbb66bc4c5e4ff
Zhongwei1986 <zhouzhongwei1986@sina.com>
2017-02-06 17:15:32 +0800

    no message

--- LocalWeather/app.js
+++ LocalWeather/app.js
@@ -12,19 +12,19 @@ var $currentWeatherArr = {
 };
 
 var $todayWeatherArr = {
-  $("#date"),
-  $("#week"),
-  $("#temperature"),
-  $('#weather-info'),
-  $('#wind-today'),
-  $('#dressing-index'),
-  $('#dressing-advice'),
-  $('#uv-index'),
-  $('#comfort-index'),
-  $('#wash-index'),
-  $('#travel-index'),
-  $('#exercise-index'),
-  $('#drying-index')
+  date_y: $("#date"),
+  week: $("#week"),
+  temperature: $("#temperature"),
+  weather: $('#weather'),
+  wind: $('#wind-today'),
+  dressing_index: $('#dressing-index'),
+  dressing_advice: $('#dressing-advice'),
+  uv_index: $('#uv-index'),
+  comfort_index: $('#comfort-index'),
+  wash_index: $('#wash-index'),
+  travel_index: $('#travel-index'),
+  exercise_index: $('#exercise-index'),
+  drying_index: $('#drying-index')
 };
 var appkey = '3e71af0ff0937226f9690f000ce27c3f';
 var cities = [];
@@ -35,7 +35,7 @@ var cities = [];
 function getCityResults() {
   var preUrl = "http://v.juhe.cn/weather/citys";
   var getUrl = preUrl + "?key=" + appkey;
-  $.getJSON(getUrl, function(data) {;
+  $.getJSON(getUrl, function(data) {
     data = JSON.parse(data);
     cities = data.result;
   });
@@ -53,6 +53,29 @@ function updateText(el, str) {
   el.text(str);
 }
 
+
+/**
+ * updateCurrentWeather 更新当天天气信息
+ * 
+ * @param {object} currentWeather 从API获得的当前天气数据
+ */
+function updateCurrentWeather(currentWeather) {
+  for (var key in currentWeather) {
+    updateText($currentWeatherArr.key, currentWeather.key);
+  }
+}
+
+/**
+ * updateTodayWeather 更新今天天气
+ * 
+ * @param {object} todayWeather
+ */
+function updateTodayWeather(todayWeather) {
+  for (var key in todayWeather) {
+    updateText($todayWeatherArr.key, todayWeather.key);
+  }
+}
+
 /**
  * getWeatherByCityName由城市名获得天气数据
  * 
@@ -63,9 +86,10 @@ function getWeatherByCityName(cityname) {
   cityname = encodeURI(cityname);
   var getUrl = api + '?format=2&cityname=' + cityname + "&key=" + appkey;
   $.getJSON(getUrl, function(data) {
-        data = JSON.parse(data);
-        var currentWeather = data.result.sk;
-        var todayWeather = data.result.today;
-        var futureWeather = data.result.future;
+    data = JSON.parse(data);
+    var currentWeather = data.result.sk;
+    var todayWeather = data.result.today;
+    var futureWeather = data.result.future;
 
-      }
+  });
+}
