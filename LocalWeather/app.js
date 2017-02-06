138e78cbf62796b553b97602a311417575813977
Zhongwei1986 <zhouzhongwei1986@sina.com>
2017-02-07 02:04:21 +0800

    no message

--- LocalWeather/app.js
+++ LocalWeather/app.js
@@ -1,43 +1,20 @@
 //全局变量
-var $window = $(window);
-var $citySearch = $('#city-search');
-var $searchBtn = $('#search-btn');
-var $cityName = $('#city-name');
-var $currentWeatherArr = {
-  temp: $('#current-temp'),
-  wind_direction: $('#wind-direction'),
-  wind_strength: $('#wind-strength'),
-  humidity: $('#humidity'),
-  time: $('#update-time')
-};
-
-var $todayWeatherArr = {
-  date_y: $("#date"),
-  week: $("#week"),
-  temperature: $("#temperature"),
-  weather: $('#weather'),
-  wind: $('#wind-today'),
-  dressing_index: $('#dressing-index'),
-  dressing_advice: $('#dressing-advice'),
-  uv_index: $('#uv-index'),
-  comfort_index: $('#comfort-index'),
-  wash_index: $('#wash-index'),
-  travel_index: $('#travel-index'),
-  exercise_index: $('#exercise-index'),
-  drying_index: $('#drying-index')
-};
 var appkey = '3e71af0ff0937226f9690f000ce27c3f';
 var cities = [];
 
-/**
+/**   
  * getCityResults 获得城市列表
  */
 function getCityResults() {
-  var preUrl = "http://v.juhe.cn/weather/citys";
-  var getUrl = preUrl + "?key=" + appkey;
-  $.getJSON(getUrl, function(data) {
-    data = JSON.parse(data);
-    cities = data.result;
+  var juheUrl = "http://v.juhe.cn/weather/citys";
+  var getUrl = juheUrl + "?dtype=&key=" + appkey + "&callback=?";
+  $.getJSON(getUrl, function(response) {
+    console.log(response);
+    $.each(response.result, function(i, e) {
+      if ($.inArray(e, cities) === -1) {
+        cities.push(e.city);
+      }
+    });
   });
   return cities;
 }
@@ -60,6 +37,13 @@ function updateText(el, str) {
  * @param {object} currentWeather 从API获得的当前天气数据
  */
 function updateCurrentWeather(currentWeather) {
+  var $currentWeatherArr = {
+    temp: $('#current-temp'),
+    wind_direction: $('#wind-direction'),
+    wind_strength: $('#wind-strength'),
+    humidity: $('#humidity'),
+    time: $('#update-time')
+  };
   for (var key in currentWeather) {
     updateText($currentWeatherArr.key, currentWeather.key);
   }
@@ -68,28 +52,108 @@ function updateCurrentWeather(currentWeather) {
 /**
  * updateTodayWeather 更新今天天气
  * 
- * @param {object} todayWeather
+ * @param {object} todayWeather 从API获得的天气数据
  */
 function updateTodayWeather(todayWeather) {
+  var $todayWeatherArr = {
+    date_y: $("#date"),
+    week: $("#week"),
+    temperature: $("#temperature"),
+    weather: $('#weather'),
+    wind: $('#wind-today'),
+    dressing_index: $('#dressing-index'),
+    dressing_advice: $('#dressing-advice'),
+    uv_index: $('#uv-index'),
+    comfort_index: $('#comfort-index'),
+    wash_index: $('#wash-index'),
+    travel_index: $('#travel-index'),
+    exercise_index: $('#exercise-index'),
+    drying_index: $('#drying-index')
+  };
   for (var key in todayWeather) {
     updateText($todayWeatherArr.key, todayWeather.key);
   }
 }
 
 /**
+ * updateFutureWeather 更新未来几天天气信息
+ * 
+ * @param {Array} futureWeather 未来几日天气
+ */
+function updateFutureWeather(futureWeather) {
+  var $futureArr = $('#future-weather .well');
+  for (var i = 0, max = futureWeather.length; i < max; i++) {
+    var dayWeather = futureWeather[i];
+    var $el = $($futureArr[i]);
+    $el.children('.future-date').text(dayWeather.date);
+    $el.children('.future-week').text(dayWeather.week);
+    $el.after(
+      '<tr><td>温度：</td><td>' + dayWeather.temperature + '</td></tr>' +
+      '<tr><td>天气：</td><td>' + dayWeather.weather + '</td></tr>' +
+      '<tr><td>风向风力：</td><td>' + dayWeather.wind + '</td><tr>'
+    );
+  }
+}
+/**
  * getWeatherByCityName由城市名获得天气数据
  * 
  * @param {string} cityname 城市名，汉字形式
  */
 function getWeatherByCityName(cityname) {
-  var preUrl = "http://v.juhe.cn/weather/index";
+  var juheUrl = "http://v.juhe.cn/weather/index";
   cityname = encodeURI(cityname);
-  var getUrl = api + '?format=2&cityname=' + cityname + "&key=" + appkey;
-  $.getJSON(getUrl, function(data) {
-    data = JSON.parse(data);
-    var currentWeather = data.result.sk;
-    var todayWeather = data.result.today;
-    var futureWeather = data.result.future;
-
+  var getUrl = juheUrl + '?format=2&cityname=' + cityname +
+    "&key=" + appkey + "&callback=?";
+  $.getJSON(getUrl, function(response) {
+    var currentWeather = response.result.sk;
+    var todayWeather = response.result.today;
+    var futureWeather = response.result.future;
+    updateCurrentWeather(currentWeather);
+    updateTodayWeather(todayWeather);
+    updateFutureWeather(futureWeather);
   });
 }
+
+/**
+ * checkCityName 检查城市名是否在API支持列表中
+ * 
+ * @returns
+ */
+function checkCityName() {
+  var cityname = $('#city-input').val();
+  if (cities.indexOf(cityname)) {
+    return ture;
+  } else {
+    return false;
+  }
+}
+
+//初始化
+$(function() {
+  getCityResults();
+  $('#search-btn').on('click', function(e) {
+    if (checkCityName()) {
+      updateText($cityName, cityname);
+      getWeatherByCityName(cityname);
+      $('.widgets').css('display', 'block');
+    } else {
+      $('#error-prompt').css('display', 'block')
+        .text('无此城市天气信息，请检查您的输入!');
+    }
+  });
+});
+
+
+//监控键盘
+$(window).keydown(function(event) {
+  if (event.which === 13) {
+    if (checkCityName()) {
+      updateText($cityName, cityname);
+      getWeatherByCityName(cityname);
+      $('.widgets').css('display', 'block');
+    } else {
+      $('#error-prompt').css('display', 'block')
+        .text('无此城市天气信息，请检查您的输入!');
+    }
+  }
+})
